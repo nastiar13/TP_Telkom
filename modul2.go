870ab6f5572552c39031e52ca335b6dc87997b69
nastiar13 <jahatmamang0@gmail.com>
2023-02-26 09:36:50 +0700

    modul 2

---
+++ modul2.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// cek_kelipatan()
+	// hasil_ujian()
+	// hitung_ukuran_persegi_panjang()
+	// hitung_diskon_dan_total_bayar()
+}
+
+func cek_kelipatan() {
+	var x int
+	fmt.Scan(&x)
+
+	if (x%2 == 0 && x%3 == 0) ||
+		(x%3 == 0 && x%5 == 0) {
+		fmt.Println("bilangan kelipatan 2 dan 3, atau kelipatan 3 dan 5")
+	} else {
+		fmt.Println("BUKAN kelipatan 2 dan 3, juga BUKAN kelipatan 3 dan 5")
+	}
+}
+
+func hasil_ujian() {
+	var i, n, n_passed, n_failed int
+	var n1, n2, n3, avg float64
+
+	fmt.Println("Berapa jumlah siswa yang nilainya akan diproses?")
+	fmt.Scan(&n)
+
+	n_passed = 0
+	n_failed = 0
+
+	for i = 1; i <= n; i++ {
+		fmt.Scan(&n1, &n2, &n3)
+		avg = (n1 + n2 + n3) / 3.0
+
+		if avg > 80.0 {
+			fmt.Println("Memenuhi syarat administratif")
+			n_passed++
+		} else {
+			fmt.Println("Tidak memenuhi syarat administratif")
+			n_failed++
+		}
+	}
+
+	fmt.Println("Jumlah siswa lolos seleksi admistrasi", n_passed)
+	fmt.Println("Jumlah siswa tidak lolos seleksi admistrasi", n_failed)
+}
+
+func hitung_ukuran_persegi_panjang() {
+	var length, width, area, perimeter, length_of_diagonal float64
+
+	fmt.Scan(&length, &width)
+
+	area = length * width
+	perimeter = 2*length + 2*width
+	length_of_diagonal = math.Sqrt(math.Pow(length, 2) + math.Pow(width, 2))
+
+	fmt.Println("Luas: ", area)
+	fmt.Println("Keliling: ", perimeter)
+	fmt.Println("Panjang diagonal: ", length_of_diagonal)
+}
+
+func hitung_diskon_dan_total_bayar() {
+	var date_of_birth, a, cd int
+	var total, discount, price float64
+
+	fmt.Scan(&date_of_birth)
+	fmt.Scan(&total)
+
+	a = date_of_birth / 1000
+	cd = date_of_birth % 100
+
+	discount = float64(a) * float64(cd)
+	price = (100 - discount) / 100.0 * total
+
+	fmt.Println("besar diskon: ", discount, "%")
+	fmt.Println("Jumlah yang dibayar: ", int(price))
+
+}
